b4a656ef34d8e7785faca4084870c15accf3ea25
Steban o <sarriaobando@gmail.com>
2025-09-16 09:15:06 -0500

    e

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Productos } from './pages/productos/productos';
+import { Formulario } from './pages/formulario/formulario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Home},
+  {path: 'nosotros', component: About},
+  {path: 'productos', component: Productos},
+  {path: 'formulario', component: Formulario}
+];
